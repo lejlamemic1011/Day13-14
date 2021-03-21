c6c8912a2bd3528ad3123df498fbb6859f439e68
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-21 15:56:10 +0100

    Add files via upload

--- ex01/scripts/index.js
+++ ex01/scripts/index.js
@@ -1,1 +1,7 @@
+var links = document.querySelectorAll("a");
 
+links.forEach(link => {
+    link.addEventListener("mouseover", function(e) {
+       e.target.offsetParent.childNodes[3].style.display = "block";
+    });
+});
