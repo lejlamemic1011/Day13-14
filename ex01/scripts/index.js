4ff3112aca00c379f5342ee0a6966c91c7db3241
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-21 20:17:13 +0100

    Add files via upload

--- ex01/scripts/index.js
+++ ex01/scripts/index.js
@@ -1,7 +1,31 @@
-var links = document.querySelectorAll("a");
+// Get the modal
+var modal = document.getElementById("myModal");
 
-links.forEach(link => {
-    link.addEventListener("mouseover", function(e) {
-       e.target.offsetParent.childNodes[3].style.display = "block";
-    });
-});
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
+
+function openModal(page) {
+  var modalIframe = document.getElementById("page");
+  modalIframe.src = page;
+  document.getElementById('myModal').style.display = 'block';
+};
